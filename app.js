5f7f37492e69e2740e7d587688d656aaa1b4aa26
Sam <samjeffcoat@gmail.com>
2021-06-08 14:42:26 -0500

    styling done

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+import axios from "axios"
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form")
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault()
+        const username = document.querySelector("input").value
+        const response = await axios.get(`https://api.github.com/users/${username}`)
+        console.log(response.data)
+    })
+})
